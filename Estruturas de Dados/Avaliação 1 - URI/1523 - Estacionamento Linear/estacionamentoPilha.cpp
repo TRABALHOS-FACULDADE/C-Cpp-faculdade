f405b45b22376be4bbed16837bf497c5f3ceae91
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-09-26 18:04:10 -0300

    Create estacionamentoPilha.cpp

---
+++ Estruturas de Dados/Avaliação 1 - URI/1523 - Estacionamento Linear/estacionamentoPilha.cpp
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define MAX 20
+
+typedef struct{
+	int chegada, saida;
+} CARRO;
+
+CARRO criarCarro(int chegada, int saida){
+	CARRO c;
+	c.chegada = chegada;
+	c.saida = saida;
+	return c;
+}
+
+typedef struct{
+	int topo;
+	CARRO dados[MAX];
+} PILHA;
+
+PILHA* criarPilha(){
+	PILHA *p;
+	p = (PILHA*)malloc(sizeof(PILHA));
+	if(p != NULL){
+		p->topo = 0;
+	}
+	return p;
+}
+
+void destruirPilha(PILHA* p){
+	free(p);
+}
+
+int pilhaVazia(PILHA* p){
+	if(p == NULL) return -1;
+	else return p->topo == 0;
+}
+
+int pilhaCheia(PILHA* p, int vagas){
+	if(p == NULL) return -1;
+	else return p->topo == vagas;
+}
+
+int empilhar(PILHA* p, CARRO c, int vagas){
+	if(p==NULL) return 0;
+	if(pilhaCheia(p, vagas)) return 0;
+	p->dados[p->topo] = c;
+	p->topo++;
+	return 1;
+}
+
+CARRO desempilhar(PILHA *p) { 
+	CARRO temp;
+	if(p != NULL || !pilhaVazia(p)){	
+		p->topo--;  // Reduz o topo para retirar
+		temp = p->dados[p->topo];
+		return temp;
+	}else{
+		return temp;
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+PILHA* p;
+CARRO c;
+/////////////////////////////////////////////////////////////////////////////////////////////////
+void fluxoEstacionamento(CARRO c, PILHA* p, int carros, int cont,  int vagas);
+void checaPossibilidade(int cont, int vagas);
+
+static int horario = 13;
+
+void fluxoEstacionamento(CARRO c, PILHA* p, int carros, int cont,  int vagas) {
+	for (int i=0; i<carros; i++) {
+		scanf("%d %d", &c.chegada, &c.saida);
+		c = criarCarro(c.chegada, c.saida);
+	}
+	
+	for (int i=1; i<horario; i++) {
+		if (i == c.chegada && !pilhaCheia(p, vagas)) {
+			empilhar(p, c, vagas);
+			cont++;
+		}
+		else if (i == c.saida && !pilhaVazia(p)) {
+			break;
+		}
+	}
+	//printf("\nCont: %d\n", cont);
+}
+
+void checaPossibilidade(int cont, int vagas) {
+	
+	printf("\n%d\n", cont);
+	printf("\n%d vagas\n", vagas);
+	
+	if (cont <= vagas) {
+		printf("Sim\n");
+	} else if (cont > vagas){
+		printf("Nao\n");
+	}
+}
+
+int main() {
+	
+int carros, vagas;
+
+while (1) {
+int cont = 0;	
+	p = criarPilha();
+	
+	scanf("%d %d", &carros, &vagas);
+	if (carros == 0 && vagas == 0) break;
+	fluxoEstacionamento(c, p, carros, cont, vagas);
+	checaPossibilidade(cont, vagas);
+	
+	destruirPilha(p);
+}
+
+return 0;
+}
